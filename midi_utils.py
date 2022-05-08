cf5aadac43d217f040b3166be62eb4d584c6a538
OhLeMatt <matthieu.verdet@epfl.ch>
2022-05-08 02:54:18 +0200

    Draft on scales as future utils

---
+++ midi_utils.py
@@ -0,0 +1,94 @@
+from cv2 import split
+from matplotlib.cbook import flatten
+import numpy as np
+from pyrsistent import b
+
+
+MIDI_IDS = np.arange(128)
+
+def to_chroma(midi_id):
+    return midi_id % 12
+
+def to_octave(midi_id):
+    return (midi_id // 12) - 1
+
+ALT_NAME_MAP = {"C": "DO", "D": "RE", "E": "MI", "F": "FA", "G": "SOL", "A": "LA", "B":"SI"} 
+
+def name_to_alt_name(name):
+    new_name = ""
+    for c in name:
+        new_name += ALT_NAME_MAP.get(c, c)
+    return new_name
+
+# Midi ids refer here to what people call chroma pitches indices, although we also want to do an analysis being irrelevant of the octave, thus 
+# we will also use the "chroma" keyword to designate the 12 different chromatic pitches.
+
+NOTES = {"C": 0, "DO": 0, 
+          "C#": 1, "DO#": 1, "Db": 1, "REb"
+          "D": 2, "RE": 2,
+          "D#": 3, "RE#": 3, "Eb": 3, "MIb": 3,
+          "E": 4, "MI": 4,
+          "F": 5, "FA": 5,
+          "F#": 6, "FA#": 6, "Gb": 6, "SOLb": 6,
+          "G": 7, "SOL": 7,
+          "G#": 8, "SOL#": 8, "Ab": 8, "LAb": 8,
+          "A": 9, "LA": 9,
+          "A#": 10, "LA#": 10, "Bb": 10, "SIb": 10,
+          "B": 11, "SI": 11}
+
+CHROMA_SHARP_NAMES = np.array(["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"])
+CHROMA_FLAT_NAMES = np.array(["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"])
+CHROMA_NAMES = np.array([(sharp_name + "/" + flat_name) if sharp_name != flat_name else sharp_name 
+                            for sharp_name, flat_name in zip(CHROMA_SHARP_NAMES, CHROMA_FLAT_NAMES)])
+CHROMA_ALT_NAMES = np.array([name_to_alt_name(name) for name in CHROMA_NAMES])
+
+MIDI_SHARP_NAMES = np.array([CHROMA_SHARP_NAMES[to_chroma(midi_id)] + np.str_(to_octave(midi_id)) if midi_id >= 12 else "" 
+                                for midi_id in MIDI_IDS])
+MIDI_FLAT_NAMES = np.array([CHROMA_FLAT_NAMES[to_chroma(midi_id)] + np.str_(to_octave(midi_id)) if midi_id >= 12 else "" 
+                                for midi_id in MIDI_IDS])
+MIDI_NAMES = np.array([(sharp_name + "/" + flat_name) if sharp_name != flat_name else sharp_name 
+                            for sharp_name, flat_name in zip(MIDI_SHARP_NAMES, MIDI_FLAT_NAMES)])
+
+MIDI_ALT_NAMES = np.array([name_to_alt_name(name) for name in MIDI_NAMES])
+
+
+def to_corrected_chroma_names(chroma_names):
+    new_chroma_names = []   
+    seen_diatonic_tones = set([name.strip("#b") for name in chroma_names if "/" not in name])
+    
+    for name in chroma_names:
+        keep = name
+        if "/" in name: 
+            splits = name.split("/")
+            throw_sharp = splits[0][:-1] in seen_diatonic_tones
+            throw_flat = splits[1][:-1] in seen_diatonic_tones
+            if throw_sharp and not throw_flat:
+                keep = splits[1]
+            elif throw_flat and not throw_sharp:
+                keep = splits[0]
+            
+            seen_diatonic_tones.add(keep.strip("#b"))
+
+        new_chroma_names.append(keep)
+        
+    return np.array(new_chroma_names)
+    
+        
+
+def to_freq(midi_id):
+    return np.power(2, (midi_id-69)/12)*440
+
+MIDI_FREQS = to_freq(MIDI_IDS)
+
+def to_midi_id(freq):
+    return np.rint(69 + np.log2(np.maximum(1,freq)/440)*12).astype(np.int16)
+
+def to_continuous_midi_id(freq):
+    return 69 + np.log2(np.maximum(1,freq)/440)*12
+
+def closest_midi_freq(f):
+    return to_freq(to_midi_id(f))
+
+def harmonics_from_midi_id(midi_id, k=8, include_id=False):
+    p = np.unique(to_midi_id(to_freq(midi_id) * np.arange(1, k+2)))
+    return p[include_id | (p != midi_id)]
---
+++ scales.py
@@ -0,0 +1,124 @@
+import numpy as np
+from scipy.fftpack import shift
+import midi_utils as mu
+
+class Scale:
+    def __init__(self, tonic_chroma, shifts):
+        self.tonic_chroma = mu.to_chroma(tonic_chroma)
+        self.shifts = np.array(shifts)
+        if len(self.shifts) != 0:
+            self.successive_shifts = Scale.compute_successive_shifts(self.shifts)
+            self.chromas = mu.to_chroma(self.successive_shifts + self.tonic_chroma)
+            self.names = mu.to_corrected_chroma_names(mu.CHROMA_NAMES[self.chromas]) 
+    
+    @staticmethod
+    def compute_successive_shifts(shifts):
+        succ = []
+        if len(shifts) == 0:
+            return succ
+        succ.append(0)
+        for s in shifts[:-1]:
+            succ.append(succ[-1] + s)
+        if succ[-1] + shifts[-1] != 12:
+            raise ValueError("shifts: " + str(shifts) + " should sum up to 12")
+        return np.array(succ)
+
+    def compute_match(self, midi_ids):
+        midi_ids = np.array(midi_ids)
+        metric = 0
+        for chroma, count in zip(*np.unique(mu.to_chroma(midi_ids), return_counts=True)):
+            if chroma in self.chromas:
+                metric += count
+        return metric/len(midi_ids)
+    
+    def __call__(self, scale_id, octave=0):
+        return octave * 12 + self.chromas[np.array(scale_id) % len(self.chromas)] if len(self.chromas) > 0 else None
+
+# DODECATONIC SCALES
+
+CHROMATIC_SHIFTS = np.ones(12, dtype=int) 
+def chromatic_scale():
+    return Scale(0, CHROMATIC_SHIFTS)
+
+# HEPTATONIC SCALES
+
+# https://en.wikipedia.org/wiki/Heptatonic_scale        
+# https://en.wikipedia.org/wiki/Diatonic_scale#:~:text=In%20music%20theory%2C%20a%20diatonic,their%20position%20in%20the%20scale.
+# https://en.wikipedia.org/wiki/Circle_of_fifths_text_table
+
+class Tetrachord:
+    # https://en.wikipedia.org/wiki/Tetrachord
+    T5 = [[3, 1, 1], [2, 2, 1], [1, 3, 1], [2, 1, 2], [1, 2, 2], [1, 1, 3]]
+    T6 = [[3, 2, 1], [3, 1, 2], [2, 2, 2], [1, 3, 2], [2, 1, 3], [1, 2, 3]]
+    
+    MAJOR = 1; MINOR = 3; HARMONIC = 2; UPPER_MINOR = 4
+    """Correct only for T5, in otherwords, either separator=2 or it is used for upper_tetra """
+
+def ditetrachordic_shifts(lower_tetra=Tetrachord.MAJOR, upper_tetra=Tetrachord.MAJOR, separator=2):
+    if separator not in [1, 2]:
+        raise ValueError("Separator is the number of halftone between the two tetrachord it can be either 1 or 2")
+    if lower_tetra < 0 or lower_tetra > 5 :
+        raise ValueError("Lower tetra can either be one of the six possibilities so in range [0, 5]")
+    if upper_tetra < 0 or upper_tetra > 5 :
+        raise ValueError("Upper tetra can either be one of the six possibilities so in range [0, 5]")
+    
+    if separator == 2:
+        shifts = Tetrachord.T5[lower_tetra]
+    else:
+        shifts = Tetrachord.T6[lower_tetra]
+    
+    return shifts + [separator] + Tetrachord.T5[upper_tetra]
+
+def ditetrachordic_scale(tonic_chroma, **kwargs):
+    return Scale(tonic_chroma, ditetrachordic_shifts(**kwargs))
+
+class Mode:
+    IONIAN = 0
+    MAJOR = 0
+    DORIAN = 1
+    PHRYGIAN = 2
+    LYDIAN = 3
+    MIXOLYDIAN = 4
+    AEOLIAN = 5
+    MINOR = 5 # Natural Minor Scale
+    LOCRIAN = 6
+
+DIATONIC_SHIFTS = ditetrachordic_shifts(Tetrachord.MAJOR, Tetrachord.MAJOR)
+HARMONIC_MAJOR_SHIFTS = ditetrachordic_shifts(Tetrachord.MAJOR, Tetrachord.HARMONIC)
+HARMONIC_MINOR_SHIFTS = ditetrachordic_shifts(Tetrachord.MINOR, Tetrachord.HARMONIC)
+MELODIC_MAJOR_SHIFTS = ditetrachordic_shifts(Tetrachord.MAJOR, Tetrachord.UPPER_MINOR)
+MELODIC_MINOR_SHIFTS = ditetrachordic_shifts(Tetrachord.MINOR, Tetrachord.MAJOR)
+GIPSY_MAJOR_SHIFTS = ditetrachordic_shifts(Tetrachord.HARMONIC, Tetrachord.HARMONIC)
+NEAPOLITAN_MINOR_SHIFTS = ditetrachordic_shifts(Tetrachord.UPPER_MINOR, Tetrachord.HARMONIC)
+ALTERED_SHIFTS = np.roll(MELODIC_MINOR_SHIFTS, -1)
+
+
+def diatonic_shifts(mode):
+    return np.roll(DIATONIC_SHIFTS, -mode)
+
+def diatonic_scale(tonic_chroma, mode=Mode.MAJOR):    
+    return Scale(tonic_chroma=tonic_chroma, shifts=diatonic_shifts(mode))
+
+
+def harmonic_scale(tonic_chroma, major=True):
+    return Scale(tonic_chroma=tonic_chroma, 
+                 shifts=HARMONIC_MAJOR_SHIFTS if major else HARMONIC_MINOR_SHIFTS)
+
+def melodic_scale(tonic_chroma, major=True):
+    return Scale(tonic_chroma=tonic_chroma, 
+                 shifts=MELODIC_MAJOR_SHIFTS if major else MELODIC_MINOR_SHIFTS)
+
+def gipsy_major_scale(tonic_chroma):
+    return Scale(tonic_chroma=tonic_chroma, shifts=GIPSY_MAJOR_SHIFTS)
+
+def neapolitan_minor_scale(tonic_chroma):
+    return Scale(tonic_chroma=tonic_chroma, shifts=NEAPOLITAN_MINOR_SHIFTS)
+
+def altered_scale(tonic_chroma):
+    return Scale(tonic_chroma=tonic_chroma, shifts=ALTERED_SHIFTS)
+
+# PENTATONIC SCALES
+
+# https://en.wikipedia.org/wiki/Pentatonic_scale
+
+# DERIVED SCALES
