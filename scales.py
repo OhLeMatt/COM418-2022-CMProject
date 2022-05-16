daf6470f6677878d1eacc7fcda9183c3218dac3a
OhLeMatt <matthieu.verdet@epfl.ch>
2022-05-16 16:13:58 +0200

    Minor changes

--- scales.py
+++ scales.py
@@ -2,14 +2,38 @@ import numpy as np
 from scipy.fftpack import shift
 import midi_utils as mu
 
+def music_to_tonic_chroma(midi_ids):
+    """Difficult and WIP heuristic to find the tonic. Needs accurate research
+
+    Args:
+        midi_ids (np.ndarray(int)): the music in midi id notes
+
+    Returns:
+        int: tonic in chroma id notes (0 to 11)
+    """
+    notes, counts = np.unique(mu.to_chroma(midi_ids), return_counts=True)
+    print(counts)
+    return notes[counts.argmax()]
+
+def music_to_chroma_shifts(midi_ids, tonic_chroma):
+    shifts = np.diff(np.unique(mu.to_chroma(midi_ids) - tonic_chroma))
+    remainder = 12 - shifts.sum()
+    if remainder > 0:
+        shifts = np.append(shifts, remainder)
+    
+    return shifts
+    
+    
+
 class Scale:
-    def __init__(self, tonic_chroma, shifts):
+    def __init__(self, tonic_chroma, shifts, name="Untitled"):
         self.tonic_chroma = mu.to_chroma(tonic_chroma)
         self.shifts = np.array(shifts)
         if len(self.shifts) != 0:
             self.successive_shifts = Scale.compute_successive_shifts(self.shifts)
             self.chromas = mu.to_chroma(self.successive_shifts + self.tonic_chroma)
-            self.names = mu.to_corrected_chroma_names(mu.CHROMA_NAMES[self.chromas]) 
+            self.names = mu.to_corrected_chroma_names(mu.CHROMA_NAMES[self.chromas])
+        self.name = name
     
     @staticmethod
     def compute_successive_shifts(shifts):
@@ -29,16 +53,21 @@ class Scale:
         for chroma, count in zip(*np.unique(mu.to_chroma(midi_ids), return_counts=True)):
             if chroma in self.chromas:
                 metric += count
+            else:
+                metric -= count
         return metric/len(midi_ids)
     
     def __call__(self, scale_id, octave=0):
         return octave * 12 + self.chromas[np.array(scale_id) % len(self.chromas)] if len(self.chromas) > 0 else None
+    
+    def __repr__(self):
+        return self.name + " Scale in " + mu.CHROMA_NAMES[self.tonic_chroma]
 
 # DODECATONIC SCALES
 
 CHROMATIC_SHIFTS = np.ones(12, dtype=int) 
 def chromatic_scale():
-    return Scale(0, CHROMATIC_SHIFTS)
+    return Scale(0, CHROMATIC_SHIFTS, "Chromatic")
 
 # HEPTATONIC SCALES
 
@@ -70,7 +99,7 @@ def ditetrachordic_shifts(lower_tetra=Tetrachord.MAJOR, upper_tetra=Tetrachord.M
     return shifts + [separator] + Tetrachord.T5[upper_tetra]
 
 def ditetrachordic_scale(tonic_chroma, **kwargs):
-    return Scale(tonic_chroma, ditetrachordic_shifts(**kwargs))
+    return Scale(tonic_chroma, ditetrachordic_shifts(**kwargs), "Ditetrachordic")
 
 class Mode:
     IONIAN = 0
@@ -82,6 +111,9 @@ class Mode:
     AEOLIAN = 5
     MINOR = 5 # Natural Minor Scale
     LOCRIAN = 6
+    
+    NAMES = ["Major", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Natural Minor", "Locrian"]
+    
 
 DIATONIC_SHIFTS = ditetrachordic_shifts(Tetrachord.MAJOR, Tetrachord.MAJOR)
 HARMONIC_MAJOR_SHIFTS = ditetrachordic_shifts(Tetrachord.MAJOR, Tetrachord.HARMONIC)
@@ -97,25 +129,26 @@ def diatonic_shifts(mode):
     return np.roll(DIATONIC_SHIFTS, -mode)
 
 def diatonic_scale(tonic_chroma, mode=Mode.MAJOR):    
-    return Scale(tonic_chroma=tonic_chroma, shifts=diatonic_shifts(mode))
-
+    return Scale(tonic_chroma=tonic_chroma, shifts=diatonic_shifts(mode), name=Mode.NAMES[mode])
 
 def harmonic_scale(tonic_chroma, major=True):
     return Scale(tonic_chroma=tonic_chroma, 
-                 shifts=HARMONIC_MAJOR_SHIFTS if major else HARMONIC_MINOR_SHIFTS)
+                 shifts=HARMONIC_MAJOR_SHIFTS if major else HARMONIC_MINOR_SHIFTS,
+                 name="Harmonic " + ("Major" if major else "Minor"))
 
 def melodic_scale(tonic_chroma, major=True):
     return Scale(tonic_chroma=tonic_chroma, 
-                 shifts=MELODIC_MAJOR_SHIFTS if major else MELODIC_MINOR_SHIFTS)
+                 shifts=MELODIC_MAJOR_SHIFTS if major else MELODIC_MINOR_SHIFTS,
+                 name="Melodic " + ("Major" if major else "Minor"))
 
 def gipsy_major_scale(tonic_chroma):
-    return Scale(tonic_chroma=tonic_chroma, shifts=GIPSY_MAJOR_SHIFTS)
+    return Scale(tonic_chroma=tonic_chroma, shifts=GIPSY_MAJOR_SHIFTS, name="Gipsy Major")
 
 def neapolitan_minor_scale(tonic_chroma):
-    return Scale(tonic_chroma=tonic_chroma, shifts=NEAPOLITAN_MINOR_SHIFTS)
+    return Scale(tonic_chroma=tonic_chroma, shifts=NEAPOLITAN_MINOR_SHIFTS, name="Neapolitan Major")
 
 def altered_scale(tonic_chroma):
-    return Scale(tonic_chroma=tonic_chroma, shifts=ALTERED_SHIFTS)
+    return Scale(tonic_chroma=tonic_chroma, shifts=ALTERED_SHIFTS, name="Altered")
 
 # PENTATONIC SCALES
 
