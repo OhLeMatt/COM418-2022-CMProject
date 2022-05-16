696e3195b0ab8a809988e587cb6190ee8db7ac83
manonmichel <manon.michel.iphone@gmail.com>
2022-05-16 16:07:56 +0200

    new midi files

--- midi_gui.py
+++ midi_gui.py
@@ -22,14 +22,14 @@ class InputMidi:
         self.Fs = 22050
         self.audio_data = music.synthesize(fs=self.Fs)
 
-    def set_file(self, file_name, path):
-        self.file_name = file_name 
-        self.path = path
-        self.playing = False
+    # def set_file(self, file_name, path):
+    #     self.file_name = file_name 
+    #     self.path = path
+    #     self.playing = False
 
-        music = PrettyMIDI(midi_file=file_name)
-        self.Fs = 22050
-        self.audio_data = music.synthesize(fs=self.Fs)
+    #     music = PrettyMIDI(midi_file=file_name)
+    #     self.Fs = 22050
+    #     self.audio_data = music.synthesize(fs=self.Fs)
 
     def play(self):
         if not self.playing:
@@ -53,9 +53,9 @@ def callback(sender, app_data, user_data):
     print("App Data: ", app_data)
     print("Filename: ", midi_file)
 
-    # music = PrettyMIDI(midi_file=midi_file)
-    # Fs = 22050
-    # audio_data = music.synthesize(fs=Fs)
+    music = PrettyMIDI(midi_file=midi_file)
+    Fs = 22050
+    audio_data = music.synthesize(fs=Fs)
 
     global inputMidi 
     inputMidi = InputMidi(midi_file, path)
@@ -81,15 +81,23 @@ def random_midi(sender, app_data, user_data):
 with dpg.file_dialog(directory_selector=False, show=False, callback=callback, id="file_dialog_id", height=300):
     dpg.add_file_extension(".mid")
     dpg.add_file_extension("", color=(150, 255, 150, 255))
-    dpg.add_file_extension("Source files (*.cpp *.h *.hpp){.cpp,.h,.hpp}", color=(0, 255, 255, 255))
-    dpg.add_file_extension(".h", color=(255, 0, 255, 255), custom_text="[header]")
-    dpg.add_file_extension(".py", color=(0, 255, 0, 255), custom_text="[Python]")
+    # dpg.add_file_extension("Source files (*.cpp *.h *.hpp){.cpp,.h,.hpp}", color=(0, 255, 255, 255))
+    # dpg.add_file_extension(".h", color=(255, 0, 255, 255), custom_text="[header]")
+    # dpg.add_file_extension(".py", color=(0, 255, 0, 255), custom_text="[Python]")
 
-with dpg.window(label="Tutorial", width=800, height=600):
+with dpg.window(label="Midi Player", width=800, height=300):
     dpg.add_button(label="File Selector", callback=lambda: dpg.show_item("file_dialog_id"))
     dpg.add_button(label="Random", callback=random_midi)
-    dpg.add_text(label="Text", default_value="Hello", tag="Text")
+    dpg.add_text(label="Text", default_value="No file selected", tag="Text")
     dpg.add_button(label="Play", callback=play_midi)
+
+# with dpg.window(label="Menu Box", width=800, height=300):
+#     list_choices = ['one', 'two', 'three', 'four']
+
+#     dpg.add_listbox(name = "Choose", items = list_choices)
+    # dpg.set_main_window_size(500,500)
+    # dpg.set_core_window
+
 
 dpg.create_viewport(title='Custom Title', width=800, height=600)
 dpg.setup_dearpygui()
