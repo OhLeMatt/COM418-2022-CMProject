25ee82a274cff80ac92f807795b8a54e6c0edfa5
manonmichel <manon.michel.iphone@gmail.com>
2022-05-16 13:41:49 +0200

    play works

--- midi_gui.py
+++ midi_gui.py
@@ -1,8 +1,21 @@
 import dearpygui.dearpygui as dpg
 import mido
-from IPython.display import Audio
 from pretty_midi import PrettyMIDI
+import sounddevice as sd
 
+
+class InputMidi: 
+
+    def __init__(self, file_name, path):
+        self.file_name = file_name 
+        self.path = path
+
+        music = PrettyMIDI(midi_file=file_name)
+        self.Fs = 22050
+        self.audio_data = music.synthesize(fs=self.Fs)
+
+    def play(self):
+        sd.play(self.audio_data, self.Fs)
 
 
 dpg.create_context()
@@ -10,22 +23,24 @@ dpg.create_context()
 def callback(sender, app_data, user_data):
     #file_name = app_data['file_name']
 
-    sf2_path = app_data['current_path']  # path to sound font file
+    path = app_data['current_path']  # path to sound font file
     midi_file = app_data['file_path_name']
     #m = mido.MidiFile(filename=file_name)
     print("Sender: ", sender)
     print("App Data: ", app_data)
     print("Filename: ", midi_file)
 
-    music = PrettyMIDI(midi_file=midi_file)
-    # waveform = music.fluidsynth(sf2_path=sf2_path)
-    # Audio(waveform, rate=44100)
-    Fs = 22050
-    audio_data = music.synthesize(fs=Fs)
+    # music = PrettyMIDI(midi_file=midi_file)
+    # Fs = 22050
+    # audio_data = music.synthesize(fs=Fs)
+
+    inputMidi = InputMidi(midi_file, path)
 
     dpg.set_value("Text", "Playing: " + app_data['file_name'])
     
-    Audio(audio_data, rate=Fs, autoplay=True)
+
+def play_midi(sender, app_data, user_data):
+
 
 
 
@@ -39,6 +54,7 @@ with dpg.file_dialog(directory_selector=False, show=False, callback=callback, id
 with dpg.window(label="Tutorial", width=800, height=600):
     dpg.add_button(label="File Selector", callback=lambda: dpg.show_item("file_dialog_id"))
     dpg.add_text(label="Text", default_value="Hello", tag="Text")
+    dpg.add_button(label="Play", callback=lambda: dpg.show_item("file_dialog_id"))
 
 dpg.create_viewport(title='Custom Title', width=800, height=600)
 dpg.setup_dearpygui()
